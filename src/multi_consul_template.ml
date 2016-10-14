16b137e74d6dd7f86592632359efc7282a176c96
Andrew Rudenko <ceo@prepor.ru>
2016-10-14 16:46:04 +0300

    clean

--- src/multi_consul_template.ml
+++ src/multi_consul_template.ml
@@ -1,9 +1,3 @@
-(*---------------------------------------------------------------------------
-   Copyright (c) 2016 Andrew Rudenko. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
-  ---------------------------------------------------------------------------*)
-
 open! Core.Std
 open! Async.Std
 
